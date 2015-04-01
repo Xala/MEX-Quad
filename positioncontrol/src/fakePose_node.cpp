2e148efa3bdbbaf5937a36c183098207d5200cbd
Jacob Greenberg <jacobgre@kth.se>
2015-04-01 17:42:18 +0200

    fake pose publisher for testing purposes

---
+++ positioncontrol/src/fakePose_node.cpp
@@ -0,0 +1,63 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <cmath>
+
+
+class fakePose
+{
+private:
+    ros::NodeHandle nh;
+    ros::Publisher pub_pose;
+    ros::Subscriber sub_pose;
+    geometry_msgs::PoseStamped pose;
+
+
+public:
+    fakePose()
+        : nh("~")
+    {
+
+        pub_pose = nh.advertise<geometry_msgs::PoseStamped>("/mavros/position/vision", 1);
+        sub_pose = nh.subscribe("/mavros/position/local", 1, &fakePose::poseCallback, this);
+    }
+
+    ~fakePose()
+    {
+    }
+
+    void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+    {
+        pose = *msg;
+        calc();
+    }
+
+
+    void calc()
+    {
+        geometry_msgs::PoseStamped desired_pose;
+        ros::Time now(0);
+        desired_pose.header.stamp = pose.header.stamp;
+        desired_pose.header.frame_id = "local_origin";
+        desired_pose.pose.position.x = 0;
+        desired_pose.pose.position.y = 0;
+        desired_pose.pose.position.z = 0.7;
+        desired_pose.pose.orientation.x = pose.pose.orientation.x;
+        desired_pose.pose.orientation.y = pose.pose.orientation.y;
+        desired_pose.pose.orientation.z = pose.pose.orientation.z;
+        desired_pose.pose.orientation.w = pose.pose.orientation.w;
+        pub_pose.publish(desired_pose);
+    }
+
+    void run()
+    {
+        ros::spin();
+    }
+};
+
+int main (int argc, char **argv)
+{
+  ros::init(argc, argv, "fakePose");
+  fakePose positioncontrol_node;
+  positioncontrol_node.run();
+}
